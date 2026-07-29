f7261909732764b479bcb8eb778b7937b69be2c2
Dmitriy Grunskiy <dmitriy.grunskiy@lemanapro.ru>
2026-07-29 15:25:04 +0500

    ci: remove snyk test from Jenkins audit scan

--- .jenkinsjobs/audit_scan.groovy
+++ .jenkinsjobs/audit_scan.groovy
@@ -3,7 +3,6 @@ def run_audit_scan(String artifactoryCreds) {
     nodeImage.inside('-u root -v /var/run/docker.sock:/var/run/docker.sock -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket -e HOME=${HOME} -w ${WORKSPACE}') {
         withCredentials([usernamePassword(credentialsId: artifactoryCreds, usernameVariable: 'ART_USERNAME', passwordVariable: 'ART_PASSWORD')]) {
             sh "npm audit --audit-level=high || echo 'npm audit completed (warnings detected)'"
-            sh "npx snyk test --severity-threshold=high || echo 'Snyk scan skipped or not available'"
         }
     }
 }
